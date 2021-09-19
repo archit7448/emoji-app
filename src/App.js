24d417c72748433959dd8f56b9359b5b48083b5d
archit singh <architsingh1064@gmail.com>
2021-09-19 17:09:13 +0530

    completed emoji app

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+import "./styles.css";
+
+var emojidictionary = {
+  "😍": "loving",
+  "😴": "sleepy",
+  "🙄": "Disblief",
+  "🤐": "secret",
+  "🤗": "hug",
+  "🍕": "pizza"
+};
+
+var emojiswehave = Object.keys(emojidictionary);
+
+export default function App() {
+  var [emojiMeaning, setemojiMeaning] = useState(" ");
+
+  function inputChangeHandler(event) {
+    var Input = event.target.value;
+    var emojiMeaning = emojidictionary[Input];
+    if (emojiMeaning === undefined) {
+      emojiMeaning = "we don't have this emoji";
+    }
+    setemojiMeaning(emojiMeaning);
+  }
+
+  function emojiHandler(emoji) {
+    var emojiMeaning = emojidictionary[emoji];
+    setemojiMeaning(emojiMeaning);
+  }
+  return (
+    <div className="App">
+      <h1>Emoji Dictionary </h1>
+      <input onChange={inputChangeHandler}></input>
+      <h2>{emojiMeaning} </h2>
+      <h3>Emoji we have</h3>
+      {emojiswehave.map(function (emoji) {
+        return <span onClick={() => emojiHandler(emoji)}>{emoji}</span>;
+      })}
+    </div>
+  );
+}
